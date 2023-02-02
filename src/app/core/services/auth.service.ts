0f778fa80d88a9b1d9122ad990aa8da77d0f8580
Daniel Gómez <danielgomezv2@gmail.com>
2023-02-02 15:53:55 -0300

    new methods added to auth service

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,11 +1,33 @@
 import { Injectable } from '@angular/core';
+import { User } from '../interfaces/user';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  private url = 'http://wallet-main.eba-ccwdurgr.us-east-1.elasticbeanstalk.com'
+
+  constructor( private http: HttpClient, private router: Router ) { }
+
+  login( user: User ) {
+    return this.http.post(`${this.url}/auth/login`, user);
+  }
+
+  register( user: User ) {
+    return this.http.post(`${this.url}/users`, user);
+  }
+
+  setSession(token: string) {
+    localStorage.setItem('user_token', token);
+  }
+
+  logout() {
+    localStorage.removeItem('user_token');
+    this.router.navigateByUrl('/login');
+  }
 
   isLoggedIn() {
     const token = localStorage.getItem('user_token');
