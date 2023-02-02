de09acc8f31bf6bc4768c936280b01d92267e0e9
Daniel Gómez <danielgomezv2@gmail.com>
2023-02-02 15:24:36 -0300

    auth selector, action, reducer and store added, appState and authState interfaces added

--- src/app/core/interfaces/user.ts
+++ src/app/core/interfaces/user.ts
@@ -1,6 +1,6 @@
 export interface User {
-    first_name: string,
-    last_name: string,
+    first_name?: string,
+    last_name?: string,
     email: string,
     password: string,
     roleId: number,
