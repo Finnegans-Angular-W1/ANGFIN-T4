daa9efa40b7cdbcc530b869b70d58735cc31b9e3
pab-lo-hub <salaberry.pablo@gmail.com>
2023-02-07 16:24:46 -0300

    Componente creado

---
+++ src/app/Componentes/enviodedinero/enviodedinero.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-enviodedinero',
+  templateUrl: './enviodedinero.component.html'
+})
+export class EnviodedineroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -37,6 +37,7 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { EffectsModule } from '@ngrx/effects';
 import { ROOT_REDUCERS } from './core/state/app.state';
 import { AuthEffects } from './core/state/effects/auth.effects';
+import { EnviodedineroComponent } from './components/enviodedinero/enviodedinero.component';
 
 
 
@@ -47,7 +48,8 @@ import { AuthEffects } from './core/state/effects/auth.effects';
     E404Component,
     LoginComponent,
     RegisterComponent,
-    HomeComponent
+    HomeComponent,
+    EnviodedineroComponent
   ],
   imports: [
     BrowserModule,
