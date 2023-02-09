993de6cefed2bb1c534d989262b6cce12b381a33
pab-lo-hub <salaberry.pablo@gmail.com>
2023-02-09 11:16:03 -0300

    Implementadas las validaciones del formulario

--- src/app/Componentes/password-reset/password-reset.component.ts
+++ src/app/Componentes/password-reset/password-reset.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
-import { Validators } from '@angular/forms';
+import { FormBuilder, Validators, FormControl, FormGroup } from '@angular/forms';
 
 import { MatFormFieldModule } from '@angular/material/form-field';
-import {MatIconModule} from '@angular/material/icon'
+import { MatIconModule } from '@angular/material/icon';
+
+// import custom validator to validate that password and confirm password fields match
+import { MustMatch } from './_helpers';
 
 @Component({
   selector: 'app-password-reset',
@@ -12,18 +14,26 @@ import {MatIconModule} from '@angular/material/icon'
 })
 export class PasswordResetComponent {
   hide = true;
+  submitted = false;
 
-  resetPasswordForm = this.fb.group({
-    email: ['', Validators.required],
-    PasswordValidation: this.fb.group({
-      password: [''],
-      confirmPassword: ['']
-    }),
+  constructor(private formBuilder: FormBuilder) { }
+
+  resetPasswordForm = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    confirmPassword: ['', [Validators.required]],
+  }, {
+    validators: MustMatch('password', 'confirmPassword')
   });
 
-  constructor(private fb: FormBuilder) { }
+  get f() { return this.resetPasswordForm.controls; }
 
   onSubmit() {
+    this.submitted = true;
+    // stop here if form is invalid
+    if (this.resetPasswordForm.invalid) {
+      return;
+    }
     // TODO: Use EventEmitter with form value
     console.log(this.resetPasswordForm.value);
   }
