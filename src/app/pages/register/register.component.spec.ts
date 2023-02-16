7cd165cee5ef73810c052a763ddbfeb35c09a1b4
pab-lo-hub <salaberry.pablo@gmail.com>
2023-02-15 22:55:23 -0300

    Test escritos

--- src/app/pages/register/register.component.spec.ts
+++ src/app/pages/register/register.component.spec.ts
@@ -1,4 +1,4 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RegisterComponent } from './register.component';
 
@@ -30,4 +30,33 @@ describe('RegisterComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it(`should have null in let error`, () => {
+    fixture = TestBed.createComponent(RegisterComponent);
+    component = fixture.debugElement.componentInstance;
+    expect(component.error).toEqual(null);
+  });
+
+  it('Check the initial form values', () => {
+    const loginFormGroup = component.form;
+    const loginFormValues = {
+      first_name: '',
+      last_name: '',
+      email: '',
+      password: '',
+    }
+    expect(loginFormGroup.value).toEqual(loginFormValues);
+  })
+
+  it('Test the number of elements in form', () => {
+    const formElement = fixture.debugElement.nativeElement.querySelector('#registerForm');
+    const inputElements = formElement.querySelectorAll('input');
+    expect(inputElements.length).toEqual(4);
+  })
+
+  it('email field validity', () => {
+    let email = component.form.controls['email']; 
+    expect(email.valid).toBeFalsy();
+  });
+
 });
