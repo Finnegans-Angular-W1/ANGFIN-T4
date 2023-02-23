2a8d88c24e287b19bf5987ec09f55dd943d34972
pab-lo-hub <salaberry.pablo@gmail.com>
2023-02-23 20:57:36 -0300

    Commit previo a modificación de funciones. agregado detalle de saldo

--- src/app/pages/plazo-fijo/plazo-fijo.component.ts
+++ src/app/pages/plazo-fijo/plazo-fijo.component.ts
@@ -3,13 +3,13 @@ import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms'
 import { Router } from '@angular/router';
 import { MatDialog } from '@angular/material/dialog';
 import { AlertComponent } from '../../shared/alerts/alert.component';
-import { plazoFijoService } from 'src/app/core/services/plazofijo.service';
 import { HttpClient } from '@angular/common/http';
 import { Store } from '@ngrx/store';
 import { AppState } from '../../core/state/app.state';
 import { selectToken, selectUser } from '../../core/state/selectors/auth.selectors';
 import { Observable } from 'rxjs';
 import { User } from 'src/app/core/interfaces/user';
+import { AccountsService } from 'src/app/core/services/accounts.service';
 
 @Component({
   selector: 'app-plazo-fijo',
@@ -20,13 +20,20 @@ export class PlazoFijoComponent implements OnInit {
   form: FormGroup;
   user: Observable<User>;
   usuario!: User;
+  accountMoney!: number;
+  userId: any;
 
-  constructor(private fb: FormBuilder, private router: Router, private dialog: MatDialog, private http: HttpClient, private store: Store<AppState>, private plazoFijoService: plazoFijoService) {
+  constructor(private fb: FormBuilder, private router: Router, private dialog: MatDialog, private http: HttpClient, private accountsService: AccountsService, private store: Store<AppState>) {
     this.user = this.store.select(selectUser);
     this.user.subscribe(user => this.usuario = user);
+    this.accountsService.getAccounts().subscribe((accounts) => this.accountMoney = accounts[0].money);
+    this.accountsService.getAccounts().subscribe((accounts) => this.userId = accounts[0].userId);
+    console.log(this.userId);
+    
+    
 
     this.form = this.fb.group({
-      importe: new FormControl('', [Validators.required, Validators.pattern(/^([0-9])*$/)]),
+      importe: new FormControl('', [Validators.required, Validators.pattern(/^([0-9])*$/), ]),
       concepto: new FormControl('', [Validators.required, Validators.pattern('^[a-zA-ZÀ-ÿ\u00f1\u00d1]+(\s*[a-zA-ZÀ-ÿ\u00f1\u00d1]*)*[a-zA-ZÀ-ÿ\u00f1\u00d1]+$')]),
     })
   };
@@ -40,13 +47,8 @@ export class PlazoFijoComponent implements OnInit {
   endDatePlazoFijo: any
   daysBetween: any
 
-  userId: any = 0;
-  ngOnInit() {
-    this.store.select(selectUser)
-      .subscribe(user => {
-        this.userId = user.id;
-      });
-  };
+
+  ngOnInit() {};
 
   plazofijohecho: any = []
 
