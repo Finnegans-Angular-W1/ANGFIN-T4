fa325eb979eedb7f70d8ed7178f99bd815c239b6
Martinez <san.martinezok@gmail.com>
2023-02-23 17:51:10 -0300

    Updated

--- src/app/shared/add-contact-dialog/add-contact-dialog.component.ts
+++ src/app/shared/add-contact-dialog/add-contact-dialog.component.ts
@@ -1,10 +1,13 @@
 import { Component, OnInit, Inject } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { FormControl, FormGroup, Validators} from '@angular/forms';
 
 interface Banco{
   value: string;
   viewValue: string;
 }
+
+
 
 @Component({
   selector: 'app-add-contact-dialog',
@@ -16,14 +19,15 @@ export class AddContactDialogComponent implements OnInit {
 
   nombre: string = '';
   entidadBancaria: string = '';
+  idBank: string = '';
 
   Bancos: Banco[] = [
-    { value: 'AlkyBank', viewValue: 'AlkyBank' },
+    { value: 'AlkyBank', viewValue: 'AlkyBank'},
     { value: 'Banco Macro', viewValue: 'Banco Macro' },
+    { value: 'Banco Nación', viewValue: 'Banco Nación' },
     { value: 'Banco Santander', viewValue: 'Banco Santander' },
     { value: 'BBVA', viewValue: 'BBVA' },
-    { value: 'Banco Nación', viewValue: 'Banco Nación' },
-    { value: 'HSBC', viewValue: 'HSBC' }
+    { value: 'HSBC', viewValue: 'HSBC' },
   ];
 
   constructor(public dialogRef: MatDialogRef<AddContactDialogComponent>, @Inject(MAT_DIALOG_DATA) public data: any) { 
@@ -37,8 +41,10 @@ export class AddContactDialogComponent implements OnInit {
     };
     this.dialogRef.close(nuevoContacto);
   }
+
+  
+
   ngOnInit(): void {
   }
-
 }
  
