0942fbe38cd52009653a9439e1097112a00a4d8f
Martinez <san.martinezok@gmail.com>
2023-02-23 05:01:16 -0300

    Fix 404

--- src/app/e404/e404.component.ts
+++ src/app/e404/e404.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-e404',
@@ -7,7 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class E404Component implements OnInit {
 
-  constructor() { }
+  onClick(){
+    console.log;
+  }
+
+  constructor(private router:Router) { 
+  }
+
 
   ngOnInit(): void {
   }
