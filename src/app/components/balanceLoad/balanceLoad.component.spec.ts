317590505bf043c9d955c4adf99618df847c672e
exequiel <conan34681335@gmail.com>
2023-02-25 17:27:44 -0300

    test component balaceloader

--- src/app/components/balanceLoad/balanceLoad.component.spec.ts
+++ src/app/components/balanceLoad/balanceLoad.component.spec.ts
@@ -4,25 +4,40 @@ import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
 import { BalanceLoadComponent } from './balanceLoad.component';
+import { ExpensesService } from 'src/app/core/services/expenses.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('BalanceLoadComponent', () => {
   let component: BalanceLoadComponent;
   let fixture: ComponentFixture<BalanceLoadComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ BalanceLoadComponent ]
+  beforeEach(async() => {
+    await TestBed.configureTestingModule({
+      declarations: [ BalanceLoadComponent ],
+      imports: [HttpClientModule],
+      providers: [ExpensesService]
     })
     .compileComponents();
-  }));
 
-  beforeEach(() => {
     fixture = TestBed.createComponent(BalanceLoadComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it('', () => {
-    
+  it('should create', () => {
+    expect(component).toBeTruthy();
+
   });
+
+  it('should render the title', () => {
+    const { debugElement } = fixture;
+    const counter = debugElement.query(By.css('app-titulos'));
+    expect(counter).toBeTruthy();
+  })
+
+  it('should render the form', () => {
+    const { debugElement } = fixture;
+    const counter = debugElement.query(By.css('app-transactions-form'));
+    expect(counter).toBeTruthy();
+  })
 });
