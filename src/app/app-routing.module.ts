d55b26906ed8b9c9eada989909accffcdf297387
Daniel Gómez <danielgomezv2@gmail.com>
2023-02-25 01:47:52 -0300

    shortcuts component added + some fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -23,6 +23,7 @@ import { PlazoFijoComponent } from './pages/plazo-fijo/plazo-fijo.component';
 import { DepositMoneyComponent } from './components/deposit-money/deposit-money.component';
 
 import { ProfileEditComponent } from './pages/profile-edit/profile-edit.component';
+import { ShortcutsComponent } from './components/shortcuts/shortcuts.component';
 
 const routes: Routes = [
   { path: '', redirectTo: 'landing', pathMatch: 'full' },
@@ -39,7 +40,7 @@ const routes: Routes = [
       { path: 'depositar-dinero', component: DepositMoneyComponent, canActivate: [AuthGuard] },
       { path: 'cambiar-password', component: PasswordResetComponent, canActivate: [AuthGuard] },
       { path: 'editar-perfil', component: ProfileEditComponent, canActivate: [AuthGuard]},
-      { path: 'transactions-form', component: TransactionsFormComponent },
+      { path: 'shortcuts', component: ShortcutsComponent },
       { path: 'currency-converter', component: CurrencyConverterComponent },
       { path: 'contactos', component:ContactsComponent, canActivate: [AuthGuard] },
       //{ path: 'operations', component: OperationsComponent },
